3c33ebcb72cba19031a2ef450dfc9d97c9eeaed5
Mavi46 <mediavisolutions@gmail.com>
2023-04-11 16:09:57 +0200

    FTP & Transcriptie

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import speech_recognition as sr
+import ftplib
 
+ftp_host = "Vraag IP aan Haci"
+ftp_user = "Vraag gebruikersnaam aan Haci"
+ftp_pass = "Vraag wachtwoord aan Haci"
 
-def main():
-    sound = 'opname.wav'
+def ftp_getter():
+    # Connect to FTP server
+    ftp = ftplib.FTP(ftp_host, ftp_user, ftp_pass)
+    # Set the current directory
+    ftp.cwd('upload')
+    # Download file from FTP server
+    with open('downloaded_opname.wav', 'wb') as f:
+        ftp.retrbinary('RETR ' + 'opname.wav', f.write)
+    transcribe_tool()
+
+def transcribe_tool():
+    sound = 'downloaded_opname.wav'
 
     r = sr.Recognizer()
 
@@ -21,4 +35,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    ftp_getter()
